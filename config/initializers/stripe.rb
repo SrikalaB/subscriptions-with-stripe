f054e1ee20b749a36c35f31d2784cb6f9a6e6fd6
Srikala Bharadwaj <srikala.bharadwaj@icicletech.com>
2016-07-27 16:25:19 +0530

    Load secret from yaml file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 config = YAML.load_file(File.expand_path("../../../config/stripe.yml",__FILE__))
-Stripe.api_key = config["#{Rails_env}"]['secret_key']
+Stripe.api_key = config["#{Rails.env}"]['secret_key']
 
